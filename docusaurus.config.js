f8c9ef8f18c5e56675654c1cf1d760b6e6fd564e
Santanu Panda <shaantalk@gmail.com>
2024-01-16 06:24:52 +0000

    Added baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: "https://shaantalk.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/portfolio_v4/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
