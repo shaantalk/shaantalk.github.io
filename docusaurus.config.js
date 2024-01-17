889376a74bdd6c4027963678da6135738682d65c
Santanu Panda <shaantalk@gmail.com>
2024-01-17 09:03:31 +0000

    social card change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/Logo.png",
       navbar: {
         title: "Santanu Panda",
         logo: {
